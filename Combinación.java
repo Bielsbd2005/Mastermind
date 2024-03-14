5d90383614c34ea1d4147e616cc71c08fb169903
Bielsbd <161064947+Bielsbd2005@users.noreply.github.com>
2024-03-15 00:00:21 +0100

    Create Combinación.java

---
+++ Combinación.java
@@ -0,0 +1,66 @@
+COMBINACION
+
+package master_Mind;
+
+public class Combinacion {
+	public static final int MIDA = 4;
+	private byte[] array_combi;
+	
+	public Combinacion() {
+		array_combi = new byte[MIDA];
+	}
+	
+	public Combinacion(int n) {
+		array_combi = new byte[n];
+	}
+	
+	public byte[] getArray_Combi() {
+		return array_combi;
+	}
+	
+	public void setArray_Combi(byte[] a) {
+		array_combi = a;
+	}
+	
+	public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : array_combi) {
+            sb.append(b).append(" ");
+        }
+        return sb.toString().trim();
+    }
+	
+	public void generaCSecreta(int min, int max) {
+		boolean tmp = true;
+		while(tmp == true) {
+			for(int i = 0; i < MIDA; i++) {
+				array_combi[i] = (byte)(Math.random() * (max - min + 1)+ min);
+			}	
+			tmp = ComprovarJugada();
+		}
+		//for(int i = 0; i < MIDA; i++)
+			//System.out.print(array_combi[i] +" ");
+
+	}
+	
+	public boolean ComprovarJugada() {
+		boolean tmp = false;
+		for(int i = 0; i < MIDA; i++) {
+			for(int j = i + 1; j < MIDA; j++) {
+				if(array_combi[i] == array_combi[j]) {
+					tmp = true;
+				}
+			}
+		}
+	return tmp;
+	}
+	
+    public boolean contiene(byte elemento) {
+        for (byte b : array_combi) {
+            if (b == elemento) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
